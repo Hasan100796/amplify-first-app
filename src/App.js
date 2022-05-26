1e4ad326dddf6855b87faa7886043ff5c783f2dd
Hasan100796 <hasana773@gmail.com>
2022-05-26 17:06:53 +0530

    changes in Full

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
   return (
     
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
+      <h1>CRUD App with Hooks Header Modification</h1>
       <div className="flex-row">
         <div className="flex-large">
           {editing ? (
